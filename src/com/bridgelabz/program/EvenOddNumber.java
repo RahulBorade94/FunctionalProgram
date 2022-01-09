9aaa9f15e31ff369db02510df0d5b80cab9a7dfb
RahulBorade94 <boraderahul94@gmail.com>
2022-01-09 17:18:21 +0530

    ADDED; Even and Odd Number

---
+++ src/com/bridgelabz/program/EvenOddNumber.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.program;
+
+import java.util.Scanner;
+
+public class EvenOddNumber {
+    public static void main(String[] args) {
+        //Variable Declaration
+        int number;
+
+        //Scanner Object for user input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Any Number :");
+        number = sc.nextInt();
+
+        //Checking for Even or Odd Number using Mod Operator
+        if(number % 2 == 0) {
+            System.out.println("The Number "+number+" is Even.");
+        } else {
+            System.out.println("The Number "+number+" is Odd.");
+        }
+    }
+
+}
